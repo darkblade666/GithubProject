f621c267ee3c54c31b289bb4399ec51ac528579e
darkblade666 <dark-blade@mail.ru>
2015-12-07 20:23:29 +0200

    1232 add branch

--- src/main/java/MyFirstTest.java
+++ src/main/java/MyFirstTest.java
@@ -2,5 +2,6 @@
 public class MyFirstTest {
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("This commit to develop branch");
     }
 }
